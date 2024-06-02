c3c694594fc044963d48b4eb7b4be2c16050d06b
Razvan <81688012+RazvanMF@users.noreply.github.com>
2024-06-02 23:20:17 +0300

    deleted legacy frontend

--- Celebration Of Capitalism - The Finale/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Celebration_Of_Capitalism___The_Finale.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
