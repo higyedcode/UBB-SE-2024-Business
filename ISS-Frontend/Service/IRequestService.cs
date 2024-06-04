1f8c49559441079f59d60d2f2b961708955e2c64
Gulin7 <gulintudor@gmail.com>
2024-06-04 18:43:56 +0300

    request

---
+++ ISS-Frontend/Service/IRequestService.cs
@@ -0,0 +1,14 @@
+﻿using ISS_Frontend.Entity;
+
+namespace ISS_Frontend.Service
+{
+    public interface IRequestService
+    {
+        List<Request> GetAllRequestsInfluencer();
+        List<Request> GetAllRequestsAddAccount();
+        Request GetRequestById(int id);
+        void AddRequest(Request request);
+        void UpdateRequest(Request request);
+        void DeleteRequest(int id);
+    }
+}
